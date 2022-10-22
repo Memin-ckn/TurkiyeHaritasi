cf268e74aefb1abb1cfc51134ae44c85f123bfb7
Furkan Polat <95643569+FurkanADS01@users.noreply.github.com>
2022-10-22 17:29:08 +0300

    Add files via upload

---
+++ Turkey/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Media.Effects;
+
+namespace Turkey
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Random rastgele=new Random();
+            LiveCharts.WinForms.GeoMap gMaps= new LiveCharts.WinForms.GeoMap();
+            Dictionary<string,double> keyValues= new Dictionary<string,double>();
+            keyValues["3065"] = rastgele.Next(0, 1000);
+            keyValues["3064"] = rastgele.Next(0, 1000);
+            keyValues["3063"] = rastgele.Next(0, 1000);
+            keyValues["3062"] = rastgele.Next(0, 1000);
+
+
+            gMaps.HeatMap = keyValues;
+            gMaps.Source = "Turkey.xml";
+
+            this.Controls.Add(gMaps);
+            gMaps.Dock= DockStyle.Fill;
+            gMaps.LandStrokeThickness = 2;
+            gMaps.Hoverable = true;
+            gMaps.BackColor=Color.Black;
+            gMaps.BackColorTransparent = false;
+
+
+        }
+    }
+}
---
+++ Turkey/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Bir bütünleştirilmiş koda ilişkin Genel Bilgiler aşağıdaki öznitelikler kümesiyle
+// denetlenir. Bütünleştirilmiş kod ile ilişkili bilgileri değiştirmek için
+// bu öznitelik değerlerini değiştirin.
+[assembly: AssemblyTitle("Turkey")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Turkey")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible özniteliğinin false olarak ayarlanması bu bütünleştirilmiş koddaki türleri
+// COM bileşenleri için görünmez yapar. Bu bütünleştirilmiş koddaki bir türe
+// erişmeniz gerekirse ComVisible özniteliğini o türde true olarak ayarlayın.
+[assembly: ComVisible(false)]
+
+// Bu proje COM'un kullanımına sunulursa, aşağıdaki GUID tür kitaplığının kimliği içindir
+[assembly: Guid("987cb1eb-f6b7-4ee0-abff-f3f7463e1c82")]
+
+// Bir derlemenin sürüm bilgileri aşağıdaki dört değerden oluşur:
+//
+//      Ana Sürüm
+//      İkincil Sürüm 
+//      Yapı Numarası
+//      Düzeltme
+//
+// Tüm değerleri belirtebilir veya varsayılan Derleme ve Düzeltme Numaralarını kullanmak için
+// aşağıda gösterildiği gibi '*' kullanabilirsiniz:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
